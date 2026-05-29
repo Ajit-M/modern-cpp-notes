f20b4290314404bb7c79ad406585458ccc329b3a
luffy <mutalik.ajit19@gmail.com>
2026-05-29 11:06:50 -0500

    Basic Data Types and User-Defined Data Type

---
+++ basicio/example/dataTypes.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+template <typename T>
+void printValueAndSize(T var){
+  std::cout << "Type is : " << typeid(var).name() << ", Value is : " << var << " , Size in Bytes : " << sizeof(var) << "\n";
+}
+
+
+int main(){
+  
+  // bool 
+  // Anything other than '0' is true 
+  bool flag{true};
+  printValueAndSize(flag);
+  
+  // int
+  int num{0};
+  printValueAndSize(num);
+
+  //float
+  float numFloat{2.3f};
+  printValueAndSize(numFloat);
+
+  //char
+  char ch{'c'};
+  printValueAndSize(ch);
+
+  //short
+  short numSh{10};
+  printValueAndSize(numSh);
+
+  //Long
+  long numLong{10};
+  printValueAndSize(numLong);
+
+  //Long Long
+  long long numLL{100000000000};
+  printValueAndSize(numLL);
+
+  //long Double
+  long double numLD{100000000000};
+  printValueAndSize(numLD);
+  
+  enum class months {
+    Jan,
+    Feb,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+
+  };
+
+  enum months birthMonth{months::Feb};
+  
+  std::cout << static_cast<int>(birthMonth) << "\n";
+
+  return 0;
+}
+
